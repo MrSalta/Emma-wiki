eafe5ec7a8df07eec477d36824e49e41fd22dcea
AKSalter <Andrew.k.salter@gmail.com>
2019-02-03 19:32:13 -0800

    initial commit

---
+++ conf/users.auth.php
@@ -0,0 +1,15 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+mrsalta:$1$4wk3zdZt$/LMd8c2jR8wysYhtdEwro1:Andrew Salter:andrew.k.salter@gmail.com:admin,user
+takamina:$1$96bl1gXb$ZAk.emdAsAmudHsTvR3q4/:Lauren Salter:lesalter@gmail.com:user
+lemur:$1$yxUV00RT$9C9yrxJXiOCPOwVEcS3OD0:Zoe Aldrich:7004golden@gmail.com:user
+raxas333:$1$iPYZA1Aq$yPjrms/NIWdWZEFPM43he1:Sara Elizabeth:raxas333@live.com:user
